40bae478e01dcde9e9466ef4a0d20bd18b6a7fd1
Sahana <Sahana@Nandakumarans-MacBook-Pro.local>
2017-06-05 19:47:46 -0400

    Initial commit

---
+++ app/src/main/java/cis/gvsu/edu/geocalculator/GeoCalculatorApp.java
@@ -0,0 +1,20 @@
+package cis.gvsu.edu.geocalculator;
+
+/**
+ * Created by Sahana on 6/5/17.
+ */
+
+public class GeoCalculatorApp extends Application {
+
+    @Override
+
+    public void onCreate() {
+
+        super.onCreate();
+
+        JodaTimeAndroid.init(this);
+
+    }
+
+
+}
